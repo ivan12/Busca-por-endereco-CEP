98584d35f8d42f1c16ca17aa66fd0e3a71a40b29
Ivan Amorim <ivan@stairs.studio>
2020-12-06 12:19:56 -0300

    fazendo downgrade para versao 6 angular

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HomeWelcomeComponent } from './home-welcome/home-welcome.component';
 
 @NgModule({
+  declarations: [HomeWelcomeComponent],
   imports: [
     RouterModule, 
     CommonModule, 
